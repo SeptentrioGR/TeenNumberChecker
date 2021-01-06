de8e958cb7c00bdf2b3681cefd415ff94f748c84
Petros Kitsaras <petros.kitsaras@gmail.com>
2021-01-06 15:40:06 +0200

    initial commit

---
+++ src/learning/thegamerurso/Main.java
@@ -0,0 +1,8 @@
+package learning.thegamerurso;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/learning/thegamerurso/TeenNumberChecker.java
@@ -0,0 +1,4 @@
+package learning.thegamerurso;
+
+public class TeenNumberChecker {
+}
